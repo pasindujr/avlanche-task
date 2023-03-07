97bb398970faab8e10bd33946b98bc92b614a241
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-03-07 10:39:26 +0530

    Changes to migrations and defined "AdminSeeder" to create the admin user when migrating the DB for the 1st time.

--- database/migrations/2023_03_05_175936_assign_mark_default_value_in_marks_table.php
+++ database/migrations/2023_03_05_175936_assign_mark_default_value_in_marks_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('marks', function (Blueprint $table) {
-            $table->string('mark')->nullable()->change();
+            $table->string('mark')->default(0)->change();
         });
     }
 
