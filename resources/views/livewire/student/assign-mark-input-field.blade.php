4e97f69e3cecc89c9bc8501df45ace0ded1fa01a
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-03-07 13:21:53 +0530

    made sure mark inputs are between 0 and 100.

--- resources/views/livewire/student/assign-mark-input-field.blade.php
+++ resources/views/livewire/student/assign-mark-input-field.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <input type="number" name="assignSubject" wire:model.lazy="mark" wire:loading.attr="disabled" x-data>
+    <input onchange="handleChange(this);" type="number" name="assignSubject" wire:model.lazy="mark" wire:loading.attr="disabled" x-data>
 </div>
--- resources/views/students/assign/marks.blade.php
+++ resources/views/students/assign/marks.blade.php
@@ -51,6 +51,17 @@
 
         </div>
     </div>
+
+    @section('scripts')
+        <script>
+            function handleChange(input) {
+                if (input.value < 0) input.value = 0;
+                if (input.value > 100) input.value = 100;
+            }
+        </script>
+    @endsection
 </x-app-layout>
+
+
 
 
