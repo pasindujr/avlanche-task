98267f4eb65bf70256739d6862beccf4ff00fe60
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-03-07 11:13:59 +0530

    Average mark calculated in student dashboard.

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -12,7 +12,25 @@ class StudentController extends Controller
         $student = User::where('id', auth()->user()->id)->first();
 
         $marks = $student->subjects;
+        $averageMarks = 0;
 
-        return view('students.student.welcome', compact('marks'));
+        // Calculate the total marks
+        $totalMarks = 0;
+        foreach ($marks as $subject) {
+            if (!$subject->pivot->mark == '') {
+                $totalMarks += $subject->pivot->mark;
+            }
+        }
+
+        // Calculate the average marks
+        if (count($marks) != 0) {
+            $averageMarks = $totalMarks / count($marks);
+
+            // Round the average marks to two decimal places
+            $averageMarks = round($averageMarks, 2);
+        }
+
+
+        return view('students.student.welcome', compact('marks', 'averageMarks'));
     }
 }
--- resources/views/students/student/welcome.blade.php
+++ resources/views/students/student/welcome.blade.php
@@ -10,8 +10,6 @@
                     </div>
                 </div>
             </div>
-
-
             <div class="px-6 py-4 border-0 rounded relative mb-4">
                 <div>
                     <div class="flex flex-col">
@@ -21,10 +19,12 @@
                                     <table class="min-w-full">
                                         <thead class="bg-white border-b">
                                         <tr>
-                                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            <th scope="col"
+                                                class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                                                 Subject
                                             </th>
-                                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            <th scope="col"
+                                                class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                                                 Mark
                                             </th>
                                         </tr>
@@ -43,6 +43,8 @@
                             </div>
                         </div>
                     </div>
+
+                    <x-basic-card :main-text="$averageMarks" :sub-text="'Average mark of assigned subjects'"/>
 
                 </div>
             </div>
