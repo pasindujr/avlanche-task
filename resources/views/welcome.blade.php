26edf7518e71c8843cc4c8d9484562016d983e1f
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-03-07 11:27:43 +0530

    Remove register routes.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -794,11 +794,6 @@
                 <a href="{{ route('login') }}"
                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
                     in</a>
-
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}"
-                       class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                @endif
             @endauth
         </div>
     @endif
@@ -854,6 +849,7 @@
                                       d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"/>
                             </svg>
                         </div>
+
 
                         <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laracasts</h2>
 
