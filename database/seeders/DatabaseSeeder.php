a389923a7db4acfaeaaa4c82dd10dfb2299f4ba6
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-11-28 11:24:01 +0530

    Updates to database seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\Department::factory(10)->create();
+        \App\Models\Position::factory(10)->create();
          \App\Models\User::factory(50)->create();
     }
 }
