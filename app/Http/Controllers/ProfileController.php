d67f769aead375df38cca395bcf9cf467e6df92d
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-11-28 17:27:50 +0530

    "Extend the profile edit feature with new fields
    The 'ProfileController' has been expanded to fetch positions and departments for the current user. The 'ProfileUpdateRequest' now expects `mobile`, `address`, `department`, and `position` as well. These changes are reflected in the profile information update form. The dashboard message now directs users to manage their profile in the profile section."

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Department;
+use App\Models\Position;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,8 +19,13 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
+        $departments = Department::all();
+        $positions = Position::all();
+
         return view('profile.edit', [
             'user' => $request->user(),
+            'departments' => $departments,
+            'positions' => $positions,
         ]);
     }
 
@@ -32,7 +40,14 @@ class ProfileController extends Controller
             $request->user()->email_verified_at = null;
         }
 
-        $request->user()->save();
+
+        User::where('id', $request->user()->id)->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'mobile_number' => $request->mobile,
+            'department_id' => $request->department,
+            'position_id' => $request->position,
+        ]);
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -18,33 +18,54 @@
         @method('patch')
 
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+            <x-input-label for="name" :value="__('Name')"/>
+            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)"
+                          required autofocus autocomplete="name"/>
+            <x-input-error class="mt-2" :messages="$errors->get('name')"/>
         </div>
 
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
-            <x-input-error class="mt-2" :messages="$errors->get('email')" />
+            <x-input-label for="email" :value="__('Email')"/>
+            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full"
+                          :value="old('email', $user->email)" required autocomplete="username"/>
+            <x-input-error class="mt-2" :messages="$errors->get('email')"/>
+        </div>
 
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
-                        {{ __('Your email address is unverified.') }}
+        <div>
+            <x-input-label for="mobile" :value="__('Mobile')"/>
+            <x-text-input id="mobile" name="mobile" type="number" class="mt-1 block w-full"
+                          :value="old('mobile', $user->mobile_number)" required autocomplete="mobile"/>
+            <x-input-error class="mt-2" :messages="$errors->get('mobile')"/>
+        </div>
 
-                        <button form="send-verification" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
-                    </p>
+        <div>
+            <x-input-label for="address" :value="__('Address')"/>
+            <x-text-input id="address" name="address" type="text" class="mt-1 block w-full"
+                          :value="old('address', $user->address)" required autocomplete="address"/>
+            <x-input-error class="mt-2" :messages="$errors->get('address')"/>
 
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600 dark:text-green-400">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
-                </div>
-            @endif
+        </div>
+
+        <!-- Departments -->
+        <div class="mt-4">
+            <x-input-label for="department" :value="__('Department')" />
+            <select required class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" name="department" id="department">
+                @foreach($departments as $department)
+                    <option @if($user->department->id == $department->id) selected @endif value="{{ $department->id }}">{{ $department->name }}</option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('department')" class="mt-2" />
+        </div>
+
+        <!-- Positions -->
+        <div class="mt-4">
+            <x-input-label for="position" :value="__('Position')" />
+            <select required class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" name="position" id="position">
+                @foreach($positions as $position)
+                    <option @if($user->position->id == $position->id) selected @endif value="{{ $position->id }}">{{ $position->name }}</option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('position')" class="mt-2" />
         </div>
 
         <div class="flex items-center gap-4">
